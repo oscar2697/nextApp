05ee257c5f1957c7685781639377cace8bea6026
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-02-08 00:57:33 -0500

    first commit

---
+++ app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+
+            <p>
+                Et velit tempor anim reprehenderit pariatur eu amet mollit aute laboris sit sint qui cupidatat. 
+                Sint irure laborum est laborum cupidatat dolore anim aliqua ipsum minim. 
+                Culpa ipsum duis aliqua eu excepteur ea aliquip ipsum laborum. 
+                Enim officia pariatur ex sint exercitation proident exercitation exercitation ea enim.
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ServicePage() {
+    return (
+        <div>
+            <h1>Services</h1>
+
+            <p>
+                Fugiat nisi anim ullamco cupidatat Lorem. 
+                Officia est velit excepteur qui laborum in consectetur. 
+                Cupidatat amet in ad id voluptate non. 
+                Lorem irure commodo nostrud et aliquip labore laborum aliqua esse esse dolore laboris. 
+                Ullamco ullamco tempor officia ipsum velit ex qui laboris tempor in qui velit ad.
+            </p>
+        </div>
+    )
+}
+
+export default ServicePage
